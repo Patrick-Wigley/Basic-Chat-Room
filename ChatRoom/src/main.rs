8c1e4567aad30b306747e7283128f5adc6f6a8fd
Patrick <patrickwigley2@gmail.com>
2023-07-01 20:10:11 +0100

    Changes - Began implementing Server side Listener

--- ChatRoom/src/main.rs
+++ ChatRoom/src/main.rs
@@ -1,3 +1,21 @@
+use std::net::{TcpListener, TcpStream};
+
+fn handle_connection(stream: TcpStream) {
+    println!("New connection");
+    
+}
+
 fn main() {
+    let listener = TcpListener::bind("127.0.0.1:80").unwrap();
+    
+    for incoming_stream in listener.incoming() {
+        match incoming_stream {
+            Ok(s) => {
+                handle_connection(s)
+            }
+            Err(e) => {  println!("ERROR DURING CONNECTION")   }
+        }
+    }
+
     println!("Hello, world!");
 }
