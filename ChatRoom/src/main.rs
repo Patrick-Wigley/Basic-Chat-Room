8d85dc0c8b24b5a9c3a1d510ee5fe052a09eea14
Patrick <patrickwigley2@gmail.com>
2023-07-02 16:07:05 +0100

    now handles a connection

--- ChatRoom/src/main.rs
+++ ChatRoom/src/main.rs
@@ -1,21 +1,28 @@
-use std::net::{TcpListener, TcpStream};
+use std::{net::{TcpListener, TcpStream}, thread};
+use std::time::Duration;
 
 fn handle_connection(stream: TcpStream) {
     println!("New connection");
-    
+        
 }
+const MAX_USERS:i32 = 5;
 
 fn main() {
     let listener_result = TcpListener::bind("127.0.0.1:80");
-    
-    let listener:TcpListener;
 
+   
     match listener_result {
         Ok(listener) => {
             println!("[SERVER]: Now Listening for connections");
             for incoming_stream in listener.incoming() {
                 match incoming_stream {
-                    Ok(s) => { handle_connection(s) },
+                    Ok(s) => { 
+                        thread::spawn(|| {
+                            handle_connection(s);
+                        });
+                      
+
+                    },
                     Err(e) => { println!("[SERVER]: ERROR DURING CONNECTION - {}", e) }
                 }
             }
@@ -24,5 +31,4 @@ fn main() {
             println!("[SERVER]: {}", e);
         }
     }
-
 }
