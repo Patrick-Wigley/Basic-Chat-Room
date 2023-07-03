c2a7f5b12615005765d0c10471e24780fcdc1bd4
Patrick <patrickwigley2@gmail.com>
2023-07-03 14:22:38 +0100

    changes

--- ChatRoom/src/main.rs
+++ ChatRoom/src/main.rs
@@ -1,11 +1,27 @@
-use std::{net::{TcpListener, TcpStream}, thread, io::Write};
+use std::{net::{TcpListener, TcpStream}, thread, io::{Write, Read}};
 use std::time::Duration;
 
 fn handle_connection(mut client: TcpStream) {
     println!("New connection");
-    let data:&[u8] = "Welcome to the server".as_bytes();
-    client.write(data);
+    let mut data:&str = "Welcome to the server";
+    //let _ = client.write(data.as_bytes());
+    
 
+    let mut val:[u32;2] = [0,10];
+
+    loop {
+        let temp = format!("user1:{:?}", val).to_string();
+        data = temp.as_str();
+        println!("Sending: {}", data);
+
+        // Add error handle - (Result) for when client disconnects
+        let _ = client.write(data.as_bytes());
+        
+        val[0] += 1;
+        val[1] += 3;
+
+        thread::sleep(Duration::from_millis(2));
+    }
 }
 const MAX_USERS:i32 = 5;
 
--- Client/src/main.rs
+++ Client/src/main.rs
@@ -3,24 +3,25 @@ use std::io::{Read, Write};
 use std::{str, string};
 
 fn main() {
-    
-    let mut stream = TcpStream::connect("127.0.0.1:80");
+    let stream = TcpStream::connect("127.0.0.1:80");
 
     match stream {
-        Ok(mut server) => {
-            println!("Connected to server");
+        Ok(mut stream) => {
+            println!("Connected to stream");
 
-            let mut raw_data = [0u8; 128];
-            server.read(&mut raw_data);
-            
-            let data = std::str::from_utf8(&raw_data);
-            
-
-            match data {
-                Ok(msg) => {
-                    println!("{}", msg);
+            let mut raw_data:[u8; 128] = [0u8; 128];
+            loop {
+                let _ = stream.read(&mut raw_data);
+                let data = std::str::from_utf8(&raw_data);
+                
+                
+                match data {
+                    Ok(msg) => {
+                        println!("{}", msg);
+                    }
+                    Err(e) => {println!("ERROR: {}", e); break;}
                 }
-                Err(e) => {println!("ERROR: {}", e)}
+
             }
         }
         Err(e) => {println!("{}", e)}
