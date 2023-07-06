8e6e31b22cb29e195e090216b5ec8c50088f520f
Patrick <patrickwigley2@gmail.com>
2023-07-06 14:59:33 +0100

    added build auto build

--- RunClient.bat
+++ RunClient.bat
@@ -1,2 +1,11 @@
-cd ./Client/target/release
-Client.exe
+@echo off
+cd ./Client
+
+if NOT exist 'target/release/Client.exe' (
+    @REM Build Release for app
+    echo Building App, this may take a moment
+    cargo build --release
+)
+
+@REM Run application
+start ./target/release/Client.exe
